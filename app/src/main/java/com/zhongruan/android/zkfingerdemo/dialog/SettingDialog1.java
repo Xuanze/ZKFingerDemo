eb7ca3b5c335d447a3e168f5fa8139ad57216ca8
LHJ <942985884@qq.com>
2018-04-08 17:54:05 +0800

    优化

---
+++ app/src/main/java/com/zhongruan/android/zkfingerdemo/dialog/SettingDialog1.java
@@ -0,0 +1,100 @@
+package com.zhongruan.android.zkfingerdemo.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.zhongruan.android.zkfingerdemo.R;
+
+
+public class SettingDialog1 extends Dialog implements View.OnClickListener {
+    private TextView contentTxt;
+    private TextView titleTxt;
+    private Button OkButton;
+    private Button NoButton;
+
+    private String content;
+    private OnCloseListener listener;
+    private String positiveName;
+    private String negativeName;
+    private String title;
+
+
+    public SettingDialog1(Context context, int themeResId, String content, OnCloseListener listener) {
+        super(context, themeResId);
+        this.content = content;
+        this.listener = listener;
+    }
+
+
+    public SettingDialog1 setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public SettingDialog1 setPositiveButton(String name) {
+        this.positiveName = name;
+        return this;
+    }
+
+    public SettingDialog1 setNegativeButton(String name) {
+        this.negativeName = name;
+        return this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pad_dialog_setting_cfcs);
+        setCanceledOnTouchOutside(false);
+        setCancelable(false);
+        initView();
+    }
+
+    private void initView() {
+        contentTxt = findViewById(R.id.tv_select_Content);
+        titleTxt = findViewById(R.id.dialog_select_title);
+        OkButton = findViewById(R.id.yes_select_Button);
+        OkButton.setOnClickListener(this);
+        NoButton = findViewById(R.id.no_select_Button);
+        NoButton.setOnClickListener(this);
+
+        contentTxt.setText(content);
+        if (!TextUtils.isEmpty(positiveName)) {
+            OkButton.setText(positiveName);
+        }
+
+        if (!TextUtils.isEmpty(negativeName)) {
+            NoButton.setText(negativeName);
+        }
+
+        if (!TextUtils.isEmpty(title)) {
+            titleTxt.setText(title);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.no_select_Button:
+                if (listener != null) {
+                    listener.onClick(this, false);
+                }
+                this.dismiss();
+                break;
+            case R.id.yes_select_Button:
+                if (listener != null) {
+                    listener.onClick(this, true);
+                }
+                break;
+        }
+    }
+
+    public interface OnCloseListener {
+        void onClick(Dialog dialog, boolean confirm);
+    }
+}
--- app/src/main/java/com/zhongruan/android/zkfingerdemo/ui/SettingActivity.java
+++ app/src/main/java/com/zhongruan/android/zkfingerdemo/ui/SettingActivity.java
@@ -2,6 +2,7 @@ package com.zhongruan.android.zkfingerdemo.ui;
 
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.zhongruan.android.zkfingerdemo.R;
 import com.zhongruan.android.zkfingerdemo.base.BaseActivity;
@@ -12,6 +13,23 @@ import com.zhongruan.android.zkfingerdemo.base.BaseActivity;
 
 public class SettingActivity extends BaseActivity implements View.OnClickListener {
     private LinearLayout mLlDataBack;
+    /**
+     * 基础设置
+     */
+    private TextView mTitleData;
+    private TextView mTvMsChange;
+    private LinearLayout mLlMsChange;
+    private TextView mTvHyfs;
+    private LinearLayout mLlHyfs;
+    private TextView mTvFingerFz;
+    private LinearLayout mLlFingerFz;
+    private TextView mTvFingerCfcs;
+    private LinearLayout mLlFingerCfcs;
+    private TextView mTvFaceXsd;
+    private LinearLayout mLlFaceXsd;
+    private TextView mTvFaceCfcs;
+    private LinearLayout mLlFaceCfcs;
+    private LinearLayout mSettingLl;
 
 
     @Override
@@ -22,6 +40,27 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
     @Override
     public void initViews() {
         mLlDataBack = findViewById(R.id.ll_data_back);
+        mLlDataBack = findViewById(R.id.ll_data_back);
+        mTitleData = findViewById(R.id.titleData);
+        mTvMsChange = findViewById(R.id.tv_ms_change);
+        mLlMsChange = findViewById(R.id.ll_ms_change);
+        mLlMsChange.setOnClickListener(this);
+        mTvHyfs = findViewById(R.id.tv_hyfs);
+        mLlHyfs = findViewById(R.id.ll_hyfs);
+        mLlHyfs.setOnClickListener(this);
+        mTvFingerFz = findViewById(R.id.tv_finger_fz);
+        mLlFingerFz = findViewById(R.id.ll_finger_fz);
+        mLlFingerFz.setOnClickListener(this);
+        mTvFingerCfcs = findViewById(R.id.tv_finger_cfcs);
+        mLlFingerCfcs = findViewById(R.id.ll_finger_cfcs);
+        mLlFingerCfcs.setOnClickListener(this);
+        mTvFaceXsd = findViewById(R.id.tv_face_xsd);
+        mLlFaceXsd = findViewById(R.id.ll_face_xsd);
+        mLlFaceXsd.setOnClickListener(this);
+        mTvFaceCfcs = findViewById(R.id.tv_face_cfcs);
+        mLlFaceCfcs = findViewById(R.id.ll_face_cfcs);
+        mLlFaceCfcs.setOnClickListener(this);
+        mSettingLl = findViewById(R.id.setting_ll);
     }
 
     @Override
@@ -39,6 +78,18 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
         switch (view.getId()) {
             case R.id.ll_data_back:
                 finish();
+                break;
+            case R.id.ll_ms_change:
+                break;
+            case R.id.ll_hyfs:
+                break;
+            case R.id.ll_finger_fz:
+                break;
+            case R.id.ll_finger_cfcs:
+                break;
+            case R.id.ll_face_xsd:
+                break;
+            case R.id.ll_face_cfcs:
                 break;
         }
     }
